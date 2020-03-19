a372682b77ceee3f1190777d7194b374b9b9ac1d
Beatatata <beata_1995@163.com>
2020-03-19 12:13:46 +0800

    num3

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,11 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
+// app.get('/', function (req, res) {
+//     res.sendFile('./dist/index.html')
+// })
 app.get('/', function (req, res) {
-    res.sendFile('./dist/index.html')
+    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 })
 
 // designates what port the app will listen to for incoming requests
